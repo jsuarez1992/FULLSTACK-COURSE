d5e6580f589314375b346c1e61039d0cc4d9d949
Jessica Suarez Cribillero SOFTS21P <jessica.suarezcribillero@centria.fi>
2022-04-18 13:19:24 +0300

    Part 1.9 ... it was quite easy :D

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -1,9 +1,17 @@
 import { useState } from 'react'
 
 const Statistics = (props) => {
+  if (props.all ===0){
+    return (
+      <>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </>
+    )  
+  }
   return (
     <>
-      <h1>Statistics</h1>
+      <h1>statistics</h1>
       <p>good {props.good}</p>
       <p>neutral {props.neutral}</p>
       <p>bad {props.bad} </p>
