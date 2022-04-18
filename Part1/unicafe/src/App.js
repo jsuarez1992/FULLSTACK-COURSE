a46c93115ed5f9c904d8ef7c75a49e6037193084
Jessica Suarez Cribillero SOFTS21P <jessica.suarezcribillero@centria.fi>
2022-04-18 14:33:06 +0300

    Part 1.11 ... careful where to put the table commands

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -25,22 +25,27 @@ const Statistics = (props) => {
   return (
     <>
       <h1>statistics</h1>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={props.all} />
-      <StatisticLine text="average" value={props.average} />
-      <StatisticLine text="positive" value={props.good / props.all + "%" } />
+      <table>
+        <tbody>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="all" value={props.all} />
+        <StatisticLine text="average" value={props.average} />
+        <StatisticLine text="positive" value={props.good / props.all + "%" } />
+        </tbody>
+      </table>
     </>
   )
 }
 
 const StatisticLine = (props) => {
   return (
-    <>
-    <p>{props.text} {props.value}</p>
-    </>
-  )
+      <tr>
+        <td>{props.text}</td>
+        <td> {props.value}</td>
+      </tr>
+    )
   }
 
 
